a0d001545a8389104183f152dcfe7808bad9486d
Ramazan <ismayiloff522@gmail.com>
2024-02-02 01:33:28 +0400

    commit

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,57 +1,48 @@
 import React, { useState } from 'react'
 export const Calculator = () => {
-    const [value, setValue] = useState(null);
+    const [value, setValue] = useState("");
 
+    const handleClick = (e) => {
+        setValue(value.concat(e.target.name));
+    }
 
     const resetFunc = () => {
-
+        setValue("");
     }
 
     const clearFunc = () => {
-
-    }
-
-    const percentFunc = () => {
-
-    }
-
-    const operatorFunc = () => {
-
-    }
-
-    const numberFunc = () => {
-
+        setValue(value.slice(0, -1));
     }
 
     const equalsFunc = () => {
-
+        setValue(eval(value))
     }
 
 
     return (
         <div className='container'>
             <div className="calculator mt-5">
-                <input type="text" className='form-control' id='result' />
+                <input type="text" className='form-control' id='result' value={value} />
                 <div className="buttons mt-4 gap-3">
-                    <button className="clear">C</button>
-                    <button className="operator plus-minus">x</button>
+                    <button className="clear" onClick={resetFunc}>C</button>
+                    <button className="operator plus-minus" onClick={clearFunc} >x</button>
                     <button className="operator percentage">%</button>
-                    <button className="operator" onClick={operatorFunc} >/</button>
-                    <button className="number" onClick={numberFunc} >7</button>
-                    <button className="number" onClick={numberFunc}>8</button>
-                    <button className="number" onClick={numberFunc}>9</button>
-                    <button className="operator" onClick={operatorFunc}>*</button>
-                    <button className="number" onClick={numberFunc}>4</button>
-                    <button className="number" onClick={numberFunc}>5</button>
-                    <button className="number" onClick={numberFunc}>6</button>
-                    <button className="operator" onClick={operatorFunc}>-</button>
-                    <button className="number" onClick={numberFunc}>1</button>
-                    <button className="number" onClick={numberFunc}>2</button>
-                    <button className="number" onClick={numberFunc}>3</button>
-                    <button className="operator" onClick={operatorFunc}>+</button>
-                    <button className="number zero" onClick={numberFunc}>0</button>
-                    <button className="decimal" onClick={numberFunc}>,</button>
-                    <button className="equals" onClick={equalsFunc}>=</button>
+                    <button name='/' className="operator" onClick={handleClick} >/</button>
+                    <button name='7' className="number" onClick={handleClick} >7</button>
+                    <button name='8' className="number" onClick={handleClick}>8</button>
+                    <button name='9' className="number" onClick={handleClick}>9</button>
+                    <button name='*' className="operator" onClick={handleClick}>*</button>
+                    <button name='4' className="number" onClick={handleClick}>4</button>
+                    <button name='5' className="number" onClick={handleClick}>5</button>
+                    <button name='6' className="number" onClick={handleClick}>6</button>
+                    <button name='-' className="operator" onClick={handleClick}>-</button>
+                    <button name='1' className="number" onClick={handleClick}>1</button>
+                    <button name='2' className="number" onClick={handleClick}>2</button>
+                    <button name='3' className="number" onClick={handleClick}>3</button>
+                    <button name='+' className="operator" onClick={handleClick}>+</button>
+                    <button name='0' className="number zero" onClick={handleClick}>0</button>
+                    <button name=',' className="decimal" onClick={handleClick}>,</button>
+                    <button className="equals" onClick={equalsFunc} >=</button>
                 </div>
             </div>
         </div>
